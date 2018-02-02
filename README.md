eea2d4b354a7d3386cf922385a1b33e53b40135d
michaelmusil <michael.musil@kiwi.com>
2018-02-02 14:52:18 +0100

    init fork

--- README.md
+++ README.md
@@ -1,42 +1,41 @@
 # Forbid specifying http(s) protocol in URLs. (no-http-protocol)
 
+## !!!forked to allow https, but disallow http!!!
+
 Use `//my.server.com` instead.
 
 ## Install
+
 npm i -D eslint-plugin-no-http-protocol
+
 ```js
     "plugins": ["no-http-protocol"],
     "rules": {
         "no-http-protocol/no-http-protocol": ["error"]
     }
-
 ```
 
 ## Rule Details
 
-This rule aims to keep URLs protocol netural.  The idea is to prevent errors typically when the site is under HTTPS and somewhere there's an HTTP URL left in the code.
+This rule aims to keep URLs protocol netural. The idea is to prevent errors typically when the site is under HTTPS and somewhere there's an HTTP URL left in the code.
 
 Examples of **incorrect** code for this rule:
 
 ```js
-
-var invalidVar = 'http://my.server.com';
-var invalidVar = 'https://my.server.com';
-$.get('http://my.site.com');
-$.get('https://my.site.com');
-let http = 'http://my.site.com';
-let https = 'https://my.site.com';
-
+var invalidVar = "http://my.server.com"
+var invalidVar = "https://my.server.com"
+$.get("http://my.site.com")
+$.get("https://my.site.com")
+let http = "http://my.site.com"
+let https = "https://my.site.com"
 ```
 
 Examples of **correct** code for this rule:
 
 ```js
-
-var valid = '//my.server.com';
-$.get('//my.site.com');
-let https = '//my.site.com';
-
+var valid = "//my.server.com"
+$.get("//my.site.com")
+let https = "//my.site.com"
 ```
 
 ### Options
@@ -48,4 +47,3 @@ None
 FTP(S) or custom protocols.
 
 ## Further Reading
-
--- tests/lib/rules/no-http-protocol.js
+++ tests/lib/rules/no-http-protocol.js
@@ -3,54 +3,44 @@
  * @author Stephen Wille
  * @contact p.stephenwille@gmail.com
  */
-'use strict';
+"use strict"
 
-var rule = require('../../../lib/rules/no-http-protocol'),
-
-    RuleTester = require('eslint').RuleTester;
+var rule = require("../../../lib/rules/no-http-protocol"),
+  RuleTester = require("eslint").RuleTester
 
 //------------------------------------------------------------------------------
 // Tests
 //------------------------------------------------------------------------------
 var errorMessage = [
+  {
+    message: 'HTTP is not allowed. Use "https://" or "//" instead.',
+    type: "Literal",
+  },
+]
+
+var ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 2015 } })
+ruleTester.run("no-http-protocol", rule, {
+  valid: [
+    { code: `var validVar = '//my.server.com'` },
+    { code: `$.get('//my.site.com')` },
+    { code: `let http = '//my.site.com'` },
+    { code: `let https = '//my.site.com'` },
+    { code: `var valid = 'https://my.server.com'` },
+    { code: `let https = 'https://my.site.com'` },
+  ],
+
+  invalid: [
     {
-        message: 'HTTP(S) is not allowed.  "Use //my.server.com" instead.',
-        type: 'Literal',
-    }];
-
-var ruleTester = new RuleTester({parserOptions: {ecmaVersion: 2015}});
-ruleTester.run('no-http-protocol', rule, {
-
-    valid: [
-        {code: `var validVar = '//my.server.com'`},
-        {code: `$.get('//my.site.com')`},
-        {code: `let http = '//my.site.com'`},
-        {code: `let https = '//my.site.com'`},
-    ],
-
-    invalid: [
-        {
-            code: `var invalidVar = 'http://my.server.com'`,
-            errors: errorMessage,
-        }, {
-            code: `var invalidVar = 'https://my.server.com'`,
-            errors: errorMessage,
-        },
-        {
-            code: `$.get('http://my.site.com')`,
-            errors: errorMessage,
-        }, {
-            code: `$.get('https://my.site.com')`,
-            errors: errorMessage,
-        },
-        {
-            code: `let http = 'http://my.site.com'`,
-            errors: errorMessage,
-        },
-        {
-            code: `let https = 'https://my.site.com'`,
-            errors: errorMessage,
-        },
-
-    ],
-});
+      code: `var invalidVar = 'http://my.server.com'`,
+      errors: errorMessage,
+    },
+    {
+      code: `$.get('http://my.site.com')`,
+      errors: errorMessage,
+    },
+    {
+      code: `let http = 'http://my.site.com'`,
+      errors: errorMessage,
+    },
+  ],
+})
